00610efd7a32091ba41135c842d366fb085d59ec
Hao Zheng <babyshung8@gmail.com>
2015-02-18 11:33:05 -0500

    Initial Commit

---
+++ TestCategoryTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestCategoryTest
+//
+//  Created by Hao Zheng on 2/18/15.
+//  Copyright (c) 2015 Alarm.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestCategoryTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestCategoryTest
+//
+//  Created by Hao Zheng on 2/18/15.
+//  Copyright (c) 2015 Alarm.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
